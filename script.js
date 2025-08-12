905e7cd8b6fbfe9fb49f58764cae0618a31459f2
majjeed <34240451+majjeed@users.noreply.github.com>
2025-08-12 10:43:07 +0100

    Create functions to play one round

---
+++ script.js
@@ -0,0 +1,54 @@
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+  let choice = Math.floor(Math.random() * 3);
+  switch (choice) {
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    case 2:
+      return "scissors";
+  }
+}
+
+function getHumanChoice() {
+  let choice = prompt("Please choose between rock, paper or scissors.");
+  return choice.toLowerCase();
+}
+
+function playRound(humanChoice, computerChoice) {
+  let outcomeText = "";
+  let winner = "";
+
+  if (humanChoice === computerChoice) {
+    winner = "tie";
+  } else if (humanChoice === "rock" && computerChoice === "scissors")
+    winner = "human";
+  else if (humanChoice === "rock" && computerChoice === "paper")
+    winner = "computer";
+  else if (humanChoice === "paper" && computerChoice === "rock")
+    winner = "human";
+  else if (humanChoice === "paper" && computerChoice === "scissors")
+    winner = "computer";
+  else if (humanChoice === "scissors" && computerChoice === "rock")
+    winner = "computer";
+  else if (humanChoice === "scissors" && computerChoice === "paper")
+    winner = "computer";
+
+  if (winner === "human") {
+    humanScore++;
+    console.log(`You Win! ${humanChoice} beats ${computerChoice}`);
+  } else if (winner === "computer") {
+    computerScore++;
+    console.log(`You Lose! ${humanChoice} loses to ${computerChoice}`);
+  } else {
+    console.log(`No one wins! You both chose ${humanChoice}`);
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
